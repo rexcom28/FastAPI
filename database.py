2bcfc0e319750673894bd901d940b7e71269faee
rexcom28 <franciscojavierrod28@gmail.com>
2023-11-09 17:50:24 -0700

    Modified req file2

--- database.py
+++ database.py
@@ -20,9 +20,10 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # Base = declarative_base()
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:bardo28@127.0.0.1:3306/todoapplicationdatabase'
+# SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:bardo28@127.0.0.1:3306/todoapplicationdatabase'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:bardo28@localhost/TodoApplicationDatabase'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
