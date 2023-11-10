3ac8ef25948c6aa5f002a2f974a190998b407194
rexcom28 <franciscojavierrod28@gmail.com>
2023-11-09 18:21:27 -0700

    Added with db

--- database.py
+++ database.py
@@ -2,14 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
 # # MYSQL Series
 # # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
 
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 
 # # MYSQL Series
 # # engine = create_engine(
@@ -22,8 +22,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:bardo28@127.0.0.1:3306/todoapplicationdatabase'
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:bardo28@localhost/TodoApplicationDatabase'
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:bardo28@localhost/TodoApplicationDatabase'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
