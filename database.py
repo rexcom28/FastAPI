f30fc0ab695faf983e814b80905ef64e0be9f227
rexcom28 <franciscojavierrod28@gmail.com>
2023-11-09 18:30:02 -0700

    Added elephantSQL

--- database.py
+++ database.py
@@ -7,9 +7,9 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 # # MYSQL Series
 # # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
 # # MYSQL Series
 # # engine = create_engine(
@@ -22,8 +22,13 @@ engine = create_engine(
 
 # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:bardo28@127.0.0.1:3306/todoapplicationdatabase'
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:bardo28@localhost/TodoApplicationDatabase'
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://kibxomns:a2fgG7YkZT64cSwfAAKKuz22dndM5qna@suleiman.db.elephantsql.com/kibxomns'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
